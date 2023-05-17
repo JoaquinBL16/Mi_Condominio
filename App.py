9136c490932f606f1bade52098efb831a593e3fd
joquinBL16 <joa.brito@duocuc.cl>
2023-05-16 23:11:59 -0400

    actualizado

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,redirect,url_for,flash
 from flask_mysqldb import MySQL
 
+
 app = Flask (__name__)
 
 #coneccion a MYSQl
@@ -65,6 +66,81 @@ def delete_reserva(id):
     mysql.connection.commit()
     flash('Reserva eliminada correctamente')
     return redirect(url_for('Index'))
+
+@app.route('/mover-datos/<string:id>')
+def mover_datos(id):
+    cur = mysql.connection.cursor()
+
+    # Consultar los datos de la tabla original
+    cur.execute('SELECT nombre_reserva, tipo_reserva, fecha_reserva FROM reservas WHERE id_reservas = {0}'.format(id))
+    datos = cur.fetchall()
+
+    # Insertar los datos en la nueva tabla
+    for fila in datos:
+        cur.execute("INSERT INTO reservas_nulas (nombre_nula, tipo_nula, fecha_nula) VALUES (%s, %s, %s)", fila)
+
+    # Eliminar los datos de la tabla original
+    cur.execute('DELETE FROM reservas WHERE id_reservas = {0}'.format(id))
+
+    # Confirmar los cambios y cerrar la conexión
+    mysql.connection.commit()
+    mysql.connection.close()
+
+    return redirect(url_for('Index'))
+
+#@app.route('/mover-datos/<id>', methods=['POST'])
+#def mover_datos():
+#    cur = mysql.connection.cursor()
+#    # Obtén los datos que deseas mover de la tabla original
+#    cur.execute("SELECT * FROM reservas WHERE id_reservas = %s", [request.form.get('id_reservas')])
+#    datos = cur.fetchall()
+#
+#    # Inserta los datos recuperados en la nueva tabla
+#    for fila in datos:
+#        cur.execute("INSERT INTO reservas_nulas (nombre_nula, tipo_nula, fecha_nula) VALUES (%s, %s, %s)", [fila[0], fila[1], fila[2]])
+#
+#    # Guarda los cambios en la base de datos
+#    mysql.connection.commit()
+#
+#    # Devuelve una respuesta exitosa al cliente
+#    return 'Datos movidos exitosamente'
+
+
+
+
+#@app.route('/mover-datos/<string:id>')
+#def mover_datos():
+#    try:
+#        # Crea un cursor para ejecutar las consultas
+#        cur = mysql.connection.cursor()
+#
+#        # Ejecuta una consulta SELECT para obtener los datos que deseas modificar
+#        select_query = "SELECT * FROM reservas WHERE id_reservas= {0}".format(id)
+#        cur.execute(select_query)
+#        datos_originales = cur.fetchall()
+#
+#        # Itera sobre los datos obtenidos y ejecuta las operaciones de inserción y eliminación en las tablas
+#        for dato_original in datos_originales:
+#            # Ejecuta una consulta INSERT para insertar los datos en la tabla de destino
+#            insert_query = "INSERT INTO reservas_nulas (nombre_nula, tipo_nula, fecha_nula) VALUES (%s, %s, %s)"
+#            cur.execute(insert_query, dato_original)
+#
+#            # Ejecuta una consulta DELETE o UPDATE para eliminar o marcar los registros que se hayan movido en la tabla original
+#            delete_query = "DELETE FROM reservas WHERE id_reservas = %s"
+#            cur.execute(delete_query, (dato_original[0],))  # asumiendo que el ID está en la primera columna
+#
+#        # Confirma los cambios realizados en la base de datos
+#            mysql.connection.commit()
+#
+#        return 'Datos movidos correctamente'
+#    except mysql.connector.Error as error:
+#        # Maneja los errores de la base de datos
+#        print('Error al mover los datos:', error)
+#        return 'Error al mover los datos'
+#    finally:
+#        # Cierra la conexión a la base de datos
+#        cur.close()
+#        mysql.connection.close()
 
 
 if __name__ == '__main__':
